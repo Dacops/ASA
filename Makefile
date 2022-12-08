74b11228c3b0d6f736a82018c68edf6e2189ced5
David Pires <79417054+Dacops@users.noreply.github.com>
2022-12-08 23:09:54 +0000

    mooshak 840

--- Makefile
+++ Makefile
@@ -2,9 +2,12 @@ CFLAGS = -O3 -std=c++11 -Wall
 
 proj: proj.cpp
 	g++ $(CFLAGS) proj.cpp -lm -o proj
+gerador: gerador.cpp
 	g++ $(CFLAGS) ladrilho_valido.cpp -lm -o gerador
 
-run: proj gerador
+run: proj
+	./proj < input.txt
+rung: proj gerador
 	./gerador args > testfile
 	./proj < testfile
 clear:
--- proj.cpp
+++ proj.cpp
@@ -6,47 +6,46 @@
 using namespace std;
 
 int combinations = 0;                       // total number of combinations.
-vector<int> _linesValues;                   // vector to save free spaces per row;
-vector<int> _occupiedColumns;               // vector to save occupied columns per row;
+int lines, columns;                         // number of lines and columns.
+bool allZeros = true;                       // check if all entries are 0
+vector<int> _linesValues;                   // vector to save free spaces per row.
+vector<int> _occupiedColumns;               // vector to save occupied columns per row (offset).
 
 void readInput() {
-    int lines, columns;                     // read number of lines and columns.
+
+    ios::sync_with_stdio(false);            // faster read speeds.
     cin >> lines;
     cin >> columns;                         // columns are not used but still need to be read.
     _linesValues.resize(lines);             // create vector that saves free spaces per row.
     _occupiedColumns.resize(lines);         // create vector that saves occupied columns per row.
     for (int i = 0; i < lines; i++) {
         cin >> _linesValues[i];
+        if (_linesValues[i] != 0) { allZeros = false; }
         _occupiedColumns[i] = 0;
     }
 }
 
-int isOver(vector<int> values) {
+bool isOver(vector<int> values) {
 
     int flag = 0;
     for(int i=0; i<(int)values.size(); i++) {
-        if (values[i]!=0 ? flag++ : flag=0);
-
-        if (flag==2) { return 0; }
+        if (values[i]>1 ? flag++ : flag=0);
+        if (flag==2) return false;
     }
-    return 1;
+    return true;
 }
 
-int freeSpace(int size, vector<int> values, vector<int> occupied) {
+bool isPossible(int pos, int size, vector<int> values, vector<int> occupied) {
 
-    int minimum = occupied[0];
-    for(int i=0; i<(int)occupied.size(); i++) { if(occupied[i]<minimum) minimum=occupied[i]; }
+    // square gets out of matrix
+    if(pos+size>(int)values.size()) return false;
 
-    for(int i=0; i<=(int)values.size()-size; i++) {
-        int space = (occupied[i]==minimum ? values[i] : 0);
-        for(int n=1; n<size; n++) {
-            if (occupied[i+n]!=minimum) { space=0; }    // 1st column of this row isn't free;
-            space = min(space, values[i+n]);
-        }
-        if(space>=size) { return i; }
+    // offset in rows below is bigger then current position
+    for(int i=pos; i<pos+size; i++) {
+        if(occupied[pos]<occupied[i]) return false;
     }
-    // error code no square of size "size" fits in this staircase.
-    return -1;
+
+    return true;
 }
 
 void getCombinations(vector<int> values, vector<int> occupied) {
@@ -68,7 +67,7 @@ void getCombinations(vector<int> values, vector<int> occupied) {
         }
         else {
             newValues[i]=0;
-            newOccupied[i]=occupied[i]+1;
+            newOccupied[i]=columns;
         }
     }
 
@@ -77,28 +76,38 @@ void getCombinations(vector<int> values, vector<int> occupied) {
 
     // Implement dynamic coding. Check if inputted vector was already calculated.
 
+    // Get minimum offset position.
+    int minOff = newOccupied[0], minPos = 0;
+    for(int i=0; i<(int)newOccupied.size(); i++) {
+        if(newOccupied[i]<minOff) {
+            minOff = newOccupied[i];
+            minPos = i;
+        }
+    }
+
     // Check if Squares of nxn size can fit in the free spots.
-    for(int n=size; n>0; n--) {
+    for(int n=newValues[minPos]; n>0; n--) {
 
-        // 1st line where a square can be placed.
-        int pos = freeSpace(n, newValues, newOccupied);
+        // is it possible to place a nxn square on this line
+        if(isPossible(minPos,n,newValues,newOccupied)) {
 
-        // error code, no square of n size fits in the matrix.
-        if(pos!=-1) {
             // new vectors need to be created, so they can be edited, newValues and newOccupied are being
             // used by all instances created in this generation.
             vector<int> newerValues;
             vector<int> newerOccupied;
-            newerValues.resize(size);
-            newerOccupied.resize(size);
-            for (int j = 0; j < size; j++) {
+            newerValues.resize((int)newValues.size());
+            newerOccupied.resize((int)newOccupied.size());
+
+            // fills new vectors.
+            for (int j=0; j<(int)newValues.size(); j++) {
                 newerValues[j] = newValues[j];
                 newerOccupied[j] = newOccupied[j];
             }
 
-            for (int j = 0; j < n; j++) {
-                newerValues[j + pos] = newerValues[j + pos] - n;
-                newerOccupied[j + pos] += n;
+            // updates new vectors.
+            for (int j=minPos; j<minPos+n; j++) {
+                newerValues[j] -= n;
+                newerOccupied[j] += n;
             }
 
             getCombinations(newerValues, newerOccupied);
@@ -110,6 +119,10 @@ void getCombinations(vector<int> values, vector<int> occupied) {
 int main() {
 
     readInput();
+    if(allZeros) {
+        cout << 0 << endl;
+        return 0;
+    }
     getCombinations(_linesValues, _occupiedColumns);
     cout << combinations << endl;
 
