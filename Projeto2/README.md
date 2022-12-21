94ae9ffd4432eb95ee1156c100834c17cbd8e686
David Pires <79417054+Dacops@users.noreply.github.com>
2022-12-21 10:43:39 +0000

    added proj2

---
+++ Projeto2/README.md
@@ -0,0 +1,11 @@
+**TODO: Figure out relation inbetween number of edges & weights.** 
+
+**Problem:** Giving a undirected graph find the heaviest path that connects all vertices.
+**Solution:**
+- **Part 1:** Sort the entries by weight, we can create a vector with n entries where each entries correspond to a weight. Then we read each edge and send each (u, v) edge to the correspondent 'w' bucket. There should be no negative or 0 edges, therefore index0 = weight1, index1 = weight2, ...
+This part of the code has a complexity of:
+$$O(n)$$
+
+- **Part 2:** Read the sorted input, start removing the higher weight edges and mark which vertices were already reached. If a edge has at leist a not marked vertice add it and mark that vertice.
+This part of the code has a complexity of:
+$$O(n)$$
---
+++ Projeto2/proj.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+vector<vector<pair<int, int>>> _weights;    // 1st vector: .size() = max weight
+                                            // 2nd vector: .size() = edges with weight index - 1
+                                            // pair: .first = u, .second = v
+vector<bool> _vertices;     // use to mark already reached vertices
+int _weight = 0;            // sum of the weights of edges taken
+int _maxW = 0;              // max edge weight
+
+
+void parseInput() {
+    // read number of vertices and edges
+    int vertices, edges;
+    cin >> vertices;
+    cin >> edges;
+
+    // initialize vertices vector
+    _vertices.resize(vertices-1);
+    for(int i=0; i<(vertices-1); i++) _vertices[i] = false;
+
+    // read edges
+    for(int i=0; i<edges; i++) {
+        int u, v, w;
+
+        cin >> u, cin.ignore(), cin >> v, cin.ignore(), cin >> w;
+        if(w>_maxW) {
+            _maxW = w;
+            _weights.resize(w);
+        }
+
+        _weights[w-1].push_back({u, v});
+    }
+}
+
+bool possible(int u, int v) {
+    if(_vertices[u-1] && _vertices[v-1]) return false;
+    _vertices[u-1] = true;
+    _vertices[v-1] = true;
+    return true;
+}
+
+void readInput() {
+    for(int i=(int)_weights.size()-1; i>=0; i--) {         // weight of edge
+        for(int j=(int)_weights[i].size()-1; j>=0; j--) {  // edges with i+1 weight
+            int u = _weights[i][j].first;
+            int v = _weights[i][j].second;
+            if(possible(u, v)) { _weight+=(i+1); cout << i+1 << endl; }
+        }
+    }
+}
+
+
+int main() {
+    parseInput();       // complexity: O(n)
+    readInput();        // complexity: O(n)
+    cout << _weight << endl;
+
+    return 0;
+}
