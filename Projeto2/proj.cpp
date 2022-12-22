4960180e4230443e6add7187d24ff206d19dbf11
David Pires <79417054+Dacops@users.noreply.github.com>
2022-12-22 13:55:34 +0000

    added Kruskal implementation
    should work in all cases

--- Projeto2/proj.cpp
+++ Projeto2/proj.cpp
@@ -7,7 +7,7 @@ using namespace std;
 vector<vector<pair<int, int>>> _weights;    // 1st vector: .size() = max weight
                                             // 2nd vector: .size() = edges with weight index - 1
                                             // pair: .first = u, .second = v
-vector<bool> _vertices;     // use to mark already reached vertices
+vector<int> _predecessors;  // identify cycles in Kruskal
 int _weight = 0;            // sum of the weights of edges taken
 int _maxW = 0;              // max edge weight
 
@@ -18,9 +18,9 @@ void parseInput() {
     cin >> vertices;
     cin >> edges;
 
-    // initialize vertices vector
-    _vertices.resize(vertices-1);
-    for(int i=0; i<(vertices-1); i++) _vertices[i] = false;
+    // initialize predecessors vector
+    _predecessors.resize(vertices-1);
+    for(int i=0; i<(vertices-1); i++) _predecessors[i] = 0;
 
     // read edges
     for(int i=0; i<edges; i++) {
@@ -36,36 +36,50 @@ void parseInput() {
     }
 }
 
-bool possible(int u, int v, bool flag) {
-    // only one vertex has been visited
-    if (flag) if(_vertices[u-1] == _vertices[v-1]) return false;
-    // one or none vertices have been visited
-    if (!flag) if(_vertices[u-1] & _vertices[v-1]) return false;
-    _vertices[u-1] = true;
-    _vertices[v-1] = true;
+bool notCycle(int u, int v) {
+
+    // same predecessors, creates cycle, do not add
+    if ( (_predecessors[u-1]==_predecessors[v-1]) && _predecessors[u-1]!=0 ) return false;
+
+    // both vertices haven't been found, create predecessor
+    if (_predecessors[u-1]==_predecessors[v-1]) {
+        int predecessor = (u>v)? u : v;
+        _predecessors[u-1] = predecessor;
+        _predecessors[v-1] = predecessor;
+    }
+
+    // different predecessors, does not create cycle
+    else {
+        int predecessor = (_predecessors[u-1]>_predecessors[v-1])? _predecessors[u-1] : _predecessors[v-1];
+        int update = (_predecessors[u-1]>_predecessors[v-1])? _predecessors[v-1] : _predecessors[u-1];
+
+        // update all vertices with predecessor 'update' (join trees)
+        for(int i=0; i<(int)_predecessors.size(); i++) {
+            if(_predecessors[i]==update) _predecessors[i]=predecessor;
+        }
+    }
+
     return true;
 }
 
-void readInput() {
-    for(int i=(int)_weights.size()-1; i>=0; i--) {         // weight of edge
+void evaluateInput() {
+    // Kruskal Implementation
+    for(int i=(int)_weights.size()-1; i>=0; i--) {         // all edges value buckets
+        for(int j=(int)_weights[i].size()-1; j>=0; j--) {  // all edges in 'i' value bucket
 
-        for(int j=(int)_weights[i].size()-1; j>=0; j--) {  // edges with i+1 weight
             int u = _weights[i][j].first;
             int v = _weights[i][j].second;
-            if(possible(u, v, true)) _weight+=(i+1);
-        }
-        for(int j=(int)_weights[i].size()-1; j>=0; j--) {  // edges with i+1 weight
-            int u = _weights[i][j].first;
-            int v = _weights[i][j].second;
-            if(possible(u, v, false)) _weight+=(i+1);
+
+            if(notCycle(u,v)) {
+                _weight += (i+1);
+            }
         }
     }
 }
 
-
 int main() {
     parseInput();       // complexity: O(n)
-    readInput();        // complexity: O(n)
+    evaluateInput();    // complexity: O(n)
     cout << _weight << endl;
 
     return 0;
